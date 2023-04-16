1accae839fde76d4a2552a382be3fcc002a29078
Anton Kovalev <kovalyov.af7@gmail.com>
2023-04-17 05:18:50 +0700

    Fix typos in comments

--- dispatcher/src/main/java/ru/relex/service/AnswerConsumer.java
+++ dispatcher/src/main/java/ru/relex/service/AnswerConsumer.java
@@ -2,7 +2,7 @@ package ru.relex.service;
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
-// Принимает ответы из rabbot-mq и передаёт в UpdateController
+// Принимает ответы из rabbit-mq и передаёт в UpdateProcessor
 // Имя сервиса-цели указывается с помощью аннотации
 public interface AnswerConsumer {
 	void consume(SendMessage sendMessage);
